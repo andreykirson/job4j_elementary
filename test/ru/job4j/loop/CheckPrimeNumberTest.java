d42f0f20ea5d0be6694875b6f6073259400847af
andreykirson <a.s.kirson@gmail.com>
2020-04-11 21:45:54 +1000

    5.5. Простое число.[#268446]

--- test/ru/job4j/loop/CheckPrimeNumberTest.java
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -5,10 +5,21 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class CheckPrimeNumberTest {
+        @Test
+        public void when5() {
+            boolean rsl = CheckPrimeNumber.check(5);
+            assertThat(rsl, is(true));
+        }
 
-    @Test
-    public void check5() {
-        boolean rsl = CheckPrimeNumber.check(6);
-        assertThat(rsl, is(false));
-    }
+        @Test
+        public void when4() {
+            boolean rsl = CheckPrimeNumber.check(4);
+            assertThat(rsl, is(false));
+        }
+
+        @Test
+        public void when1() {
+            boolean rsl = CheckPrimeNumber.check(1);
+            assertThat(rsl, is(false));
+        }
 }
