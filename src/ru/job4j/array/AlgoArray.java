0b07abaad559ecc42fec2094b3b58ad9c102dd0b
andreykirson <a.s.kirson@gmail.com>
2020-04-14 22:05:30 +1000

    6.2. Упорядочить массив.[#268399]

---
+++ src/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int minId = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minId = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = min;
+            array[minId] = temp;
+            System.out.println(array[i]);
+        }
+
+    }
+}
