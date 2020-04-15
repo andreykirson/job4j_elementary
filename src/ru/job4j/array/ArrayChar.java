e13174fce82deaf0935e06838a4ece7e10ccc5ac
andreykirson <a.s.kirson@gmail.com>
2020-04-15 15:21:25 +1000

    6.4. Слово начинается с ...[#268378]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -10,4 +10,5 @@ public class ArrayChar {
         }
         return result;
     }
+
 }
--- test/ru/job4j/array/ArrayCharTest.java
+++ test/ru/job4j/array/ArrayCharTest.java
@@ -20,4 +20,5 @@ public class ArrayCharTest {
         boolean result = ArrayChar.startsWith(word, pref);
         assertThat(result, is(false));
     }
+
 }
