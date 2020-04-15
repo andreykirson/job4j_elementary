af8c7fe18cc779c24e77bf337d485c5ba6cfc227
andreykirson <a.s.kirson@gmail.com>
2020-04-15 14:20:58 +1000

    6.1. Классический поиск перебором.[#268376]

--- src/ru/job4j/array/AlgoArray.java
+++ src/ru/job4j/array/AlgoArray.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class AlgoArray {
     public static void main(String[] args) {
-        int[] array = new int[] {5, 3, 2, 1, 4};
+        int[] array = new int[]{5, 3, 2, 1, 4};
 
         for (int i = 0; i < array.length; i++) {
             int min = array[i];
