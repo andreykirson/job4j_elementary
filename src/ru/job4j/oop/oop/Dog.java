35de113c766a8c15a08277fb3c3b5010ca30b49f
andreykirson <a.s.kirson@gmail.com>
2020-04-18 11:33:47 +1000

    1.1. Создание объекта[#268538]

---
+++ src/ru/job4j/oop/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop.oop;
+
+public class Dog {
+    public  static void dog() {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
