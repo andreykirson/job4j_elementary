466b59f96e2644d912901566794be07c1128918d
andreykirson <a.s.kirson@gmail.com>
2020-04-19 09:16:14 +1000

    3. Перегрузить метод max для трех чисел[#268537]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,10 +1,22 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        int result = left > right ? left : right;
+    public static int maxMultiple(int first, int second) {
+        int result = first > second ? first : second;
         return result;
     }
+
+    public static int maxMultiple(int first, int second, int third) {
+        return maxMultiple(first,
+                maxMultiple(second, third));
+    }
+
+    public static int maxMultiple(int first, int second, int third, int fourth) {
+    return maxMultiple(first,
+            maxMultiple(second, third),
+            maxMultiple(third, fourth));
+    }
+
 }
 
 
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -8,18 +8,36 @@ public class MaxTest {
 
     @Test
     public void whenMax1To2Then2() {
-            int result = Max.max(1, 2);
+            int result = Max.maxMultiple(1, 2);
             assertThat(result, is(2));
         }
     @Test
     public void whenMax2To1Then1() {
-        int result = Max.max(3,1);
+        int result = Max.maxMultiple(3,1);
         assertThat(result, is(3));
     }
 
     @Test
     public void when1Equal2ThenAny() {
-        int result = Max.max(4,4);
+        int result = Max.maxMultiple(4,4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenForth() {
+    int result = Max.maxMultiple(1, 2, 3, 4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenThree() {
+        int result = Max.maxMultiple(1, 2, 3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void AllFourTheSame() {
+        int result = Max.maxMultiple(4, 4, 4, 4);
         assertThat(result, is(4));
     }
 }
