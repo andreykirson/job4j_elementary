859a8d0f5bec16fe7c626ecdc9c116dd3872b84d
andreykirson <a.s.kirson@gmail.com>
2020-04-18 15:44:26 +1000

    1.3. Вызов метода с аргументами.[#268541]

---
+++ src/ru/job4j/oop/Story.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop;
+
+public class Story {
+    public static void main(String[] args) {
+
+        Ball ball = new Ball();
+        Wolf wolf = new Wolf();
+        Hare hare = new Hare();
+        Fox fox = new Fox();
+
+        ball.runaway(hare, wolf);
+        fox.eat(ball);
+        wolf.take(ball);
+        hare.take(ball);
+    }
+}
+
