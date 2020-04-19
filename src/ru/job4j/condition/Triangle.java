47c44f90123e2b29f944ad7e02a5d0cf17c7d119
andreykirson <a.s.kirson@gmail.com>
2020-04-19 21:09:00 +1000

    2.2. Рефакторинг - Площадь треугольника.[#268535]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -1,8 +1,74 @@
 package ru.job4j.condition;
 
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
-      boolean result = (ab + ac) > bc && (ac + bc) > ab && (ab + bc) > ac ? true : false;
+    private Point first;
+    private Point second;
+    private Point third;
+    double a, b, c;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+        this.a = first.distance(second); // правильно ли я сделал чтобы не тащить в каждый метод координаты точек вершин треугольника сразу в треугольнике сделал поля длина стороны?
+        this.b = first.distance(third);
+        this.c = second.distance(third);
+    }
+
+    /**
+     * Метод вычисления периметра по длинам сторон.
+     *
+     * Формула.
+     *
+     * (a + b + c) / 2
+     *
+     * @param a расстояние между точками a b
+     * @param b расстояние между точками a c
+     * @param c расстояние между точками b c
+     * @return Периметр.
+     */
+    public double period(double a, double b, double c) {
+        if (exist(a, b, c)) {
+            double p = (0.5 * (a + b + c));
+            return p;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Метод должен вычислить площадь треугольника.
+     *
+     * Формула.
+     *
+     * √ p *(p - ab) * (p - ac) * (p - bc)
+     *
+     * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
+     *
+     * @return Вернуть площадь, если треугольник существует или -1.
+     */
+    public double area() {
+        double rsl = -1;
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     *
+     * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
+     *
+     * @param a Длина от точки a b.
+     * @param b Длина от точки a c.
+     * @param c Длина от точки b c.
+     * @return
+     */
+    static boolean exist(double a, double c, double b) {
+        boolean result = (a + c) > c && (b + c) > a && (a + c) > a ? true : false;
         return result;
     }
+
 }
--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -1,5 +1,7 @@
+
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -7,15 +9,46 @@ import static org.junit.Assert.assertThat;
 public class TriangleTest {
 
     @Test
-    public void whenExist() {
-        boolean result = Triangle.exist(2,2,2);
+    public void tExist() {
+        Point first = new Point(1,1);
+        Point second = new Point(3,5);
+        Point third = new Point(4,1);
+        Triangle testTriangle = new Triangle(first, second, third);
+        boolean result = Triangle.exist(testTriangle.a, testTriangle.b, testTriangle.c);
         assertThat(result, is(true));
     }
 
     @Test
     public void whenNotExist() {
-        boolean result = Triangle.exist(2,2,0);
+        Point first = new Point(1,10);
+        Point second = new Point(10,1);
+        Point third = new Point(10,1);
+        Triangle testTriangle = new Triangle(first, second, third);
+        boolean result = Triangle.exist(testTriangle.a, testTriangle.b, testTriangle.c); // Почему  java хочет сделать метод exist, т.к если не сделать то выдаёт ошибку Error non-static method exist(double,double,double) cannot be referenced from a static context
         assertThat(result, is(false));
     }
 
+
+    @Test
+    public void period() {
+        Point first = new Point(1,1);
+        Point second = new Point(3,5);
+        Point third = new Point(4,1);
+        Triangle testTriangle = new Triangle(first, second, third);
+        double rsl = testTriangle.period(testTriangle.a, testTriangle.b, testTriangle.c);
+        double expected = 5.79;
+        Assert.assertEquals(expected, rsl,  0.01);
+    }
+
+
+    @Test
+    public void area() {
+        Point first = new Point(1,10);
+        Point second = new Point(2,5);
+        Point third = new Point(4,20);
+        Triangle testTriangle = new Triangle(first, second, third);
+        double expected = 12.49;
+        double rsl = testTriangle.area(); // как все необходимые параметры попали в метод area я ведь не передавал туда в явном виде ничего?
+        Assert.assertEquals(expected, rsl,  0.01);
+    }
 }
